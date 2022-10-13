d70197569f98dcda71a72f55ed72f05fc3292ec7
Harrison Bishop <harrisonbishsp@gmail.com>
2022-10-13 12:19:59 +0100

    added switch statement for userChoice variable, letting user choose rock, paper or scissors and displaying in console. Haven't added case insensitivity

--- index.js
+++ index.js
@@ -1,10 +1,49 @@
+function playerSelection() {
+    let userChoice = prompt("Rock, Paper or Scissors:");
+
+ switch(userChoice) {
+    case "rock":
+        console.log("Rock");
+        break;
+    case "paper":
+        console.log("Paper");
+        break;
+    case "scissors":
+        console.log("Scissors");
+ }
+
+}
+
+playerSelection()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 function getComputerChoice() {
 
 const options = ['Rock', 'Paper', 'Scissors'];
 let choice = options[Math.floor(Math.random() * options.length)];
 console.log(choice);
-    
-}
+    }
 
+
+
+
+
+
+
+    
 
