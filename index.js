135635d5724dd78fb3ee55475733c23b9edf45a6
Harrison Bishop <harrisonbishsp@gmail.com>
2022-10-19 08:49:16 +0100

    reverted back to computer answer only

--- index.js
+++ index.js
@@ -1,55 +1,11 @@
 
-
-
-function playerSelection() {
-    let userChoice = prompt("Rock, Paper or Scissors:");
-
- switch(userChoice.toLowerCase()) {
-    case "rock".toLowerCase():
-        console.log("Rock");
-        break;
-    case "paper".toLowerCase():
-        console.log("Paper");
-        break;
-    case "scissors".toLowerCase():
-        console.log("Scissors");
-        break;
-    default:
-        console.log("I don't Know that one!:s");  
- }
-
-}
-
-playerSelection()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 function getComputerChoice() {
 
-const options = ['Rock', 'Paper', 'Scissors'];
-let choice = options[Math.floor(Math.random() * options.length)];
-console.log(choice);
-    }
+    const options = ['Computer: Rock', 'Computer: Paper', 'Computer: Scissors'];
+    const choice = options[Math.floor(Math.random() * options.length)];
 
-
-
-
-
-
-
+   return console.log(choice)
+}
     
 
+getComputerChoice()
