d07727ab2c29fb6529cea0c1512f212a0e584ed3
Harrison Bishop <harrisonbishsp@gmail.com>
2022-11-02 14:03:23 +0000

    created global selector for human choices based off button click. added global const for player scoreboard. changed js to run with DOM instead of through console. Added reset button

--- index.js
+++ index.js
@@ -1,75 +1,110 @@
 
-
+//global scores
 let playerScore = 0;
 let computerScore = 0;
+let drawScore = 0
 let roundWinner = '';
 
+//global selector for human choice
+const ROCK = document.querySelector('#ROCK');
+const PAPER = document.querySelector('#PAPER');
+const SCISSORS = document.querySelector('#SCISSORS');
 
+//global selector for scoreboards
+const playerCount = document.querySelector('#playerScore');
+const computerCount = document.querySelector('#computerScore');
+const drawCount = document.querySelector('#roundsDrawn');
+const reset = document.querySelector('#reset')
+
+//getting a randomised choice from the computer
 function getComputerChoice() {
 
-    const options = ["ROCK", "SCISSORS", "PAPER"];
-    const choice = options[Math.floor(Math.random() * options.length)];
+const options = ["ROCK", "SCISSORS", "PAPER"];
+const choice = options[Math.floor(Math.random() * options.length)];
 
-   return choice;
+return choice;
 }
 
 
-function getHumanChoice() {
-    const question = prompt("Rock, Paper or Scissors?", "").toUpperCase();
-    if (question === "ROCK"){
-        return "ROCK";
-    }else if (question === "PAPER"){
-        return "PAPER";
-    } else if (question === "SCISSORS"){
-        return "SCISSORS";
-    }else{
-        return console.log("IDK that one");
-    }
+//event listeners for rock, paper scissors buttons
+//and decide player selection value.
+ROCK.addEventListener('click', function(){
+let playerSelection = "ROCK";
+let computerSelection = getComputerChoice();
+playRound(playerSelection, computerSelection);
+});
+
+SCISSORS.addEventListener('click', function(){
+let playerSelection = "SCISSORS";
+let computerSelection = getComputerChoice();
+playRound(playerSelection, computerSelection);
+});
+
+PAPER.addEventListener('click', function(){
+let playerSelection = "PAPER";
+let computerSelection = getComputerChoice();
+playRound(playerSelection, computerSelection);
+});
+
+//event listener for resetting the game
+reset.addEventListener('click', function(){
+    resetGame()
+    });
     
-}
+// plays one round based off the computer
+// and human choice
 
  function playRound(playerSelection, computerSelection) {
-
-playerSelection = getHumanChoice();
-computerSelection = getComputerChoice();
+ computerSelection = getComputerChoice();
 
 
  if (playerSelection === computerSelection){
+  drawScore++;  
   roundWinner = 'tie';
+  drawCount.innerHTML = 'The number of draws is currently:' + drawScore
 }else if (
    (playerSelection === "ROCK" && computerSelection === "SCISSORS") ||
    (playerSelection === "SCISSORS" && computerSelection === "PAPER") ||
    (playerSelection === "PAPER" && computerSelection === "ROCK")
    ){
-      playerScore++;
-      roundWinner = 'player';
+    playerScore++;
+    roundWinner = 'player';
+    playerCount.innerHTML = 'Your score is currently:' + playerScore;
 }else if (
    (computerSelection === "ROCK" && playerSelection === "SCISSORS") ||
    (computerSelection === "SCISSORS" && playerSelection === "PAPER") ||
    (computerSelection === "PAPER" && playerSelection === "ROCK")
    ){
-      computerScore++;
-      roundWinner = 'computer';
+    computerScore++;
+    roundWinner = 'computer';
+    computerCount.innerHTML = 'The opposition score is currently:' + computerScore;
    }
-   console.log(roundWinner);
+
+
+if (playerScore === 5){
+    return alert('You win!')
+}else if(computerScore === 5){
+    return alert('you lose!')
+}
  }
 
-
-
-
-
-
-
-function game(){
-    for (let i = 0; i < 5; i++){
-playRound()}
-
-   if (playerScore > computerScore){
-      return console.log("YOU WIN!");
-   }else if (playerScore < computerScore){
-      return console.log("YOU LOSE!")
-   }else{ return console.log("It's a tie!!")}
-
+function resetGame(){
+playerCount.innerHTML = "Your score is currently: 0";
+computerCount.innerHTML = "The opposition score is currently: 0";
+drawCount.innerHTML = "The number of draws is currently: 0";
+playerScore = 0;
+computerScore = 0;
+drawScore = 0;
 }
 
-game()
+
+//play intro to website
+const splash = document.querySelector('.splash');
+document.addEventListener('DOMContentLoaded', (e)=>{
+    setTimeout(()=>{
+        splash.classList.add('display-none')
+
+
+    }, 2000);
+        
+})
